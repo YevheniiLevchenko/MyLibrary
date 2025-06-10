763275ab99fd130d7e3d9b81c35007f82414a3eb
YevheniiLevchenko <yevhenii@sportsbox.ai>
2025-06-10 17:58:55 +0300

    Fix buttons init access

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -5,6 +5,13 @@ public struct DefaultButton: View {
     let systemImage: String
     let color: Color
     let action: () -> Void
+    
+    public init(title: String, systemImage: String, color: Color, action: @escaping () -> Void) {
+        self.title = title
+        self.systemImage = systemImage
+        self.color = color
+        self.action = action
+    }
 
     public var body: some View {
         Button(action: action) {
